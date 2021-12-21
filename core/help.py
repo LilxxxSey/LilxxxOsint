80d886fb4ad24175c3ddb4c3ad4d170609117fe6
! Lil x Sey <92247496+LilxxxSey@users.noreply.github.com>
2021-12-20 20:35:25 -0700

    Update help.py

--- core/help.py
+++ core/help.py
@@ -1,7 +1,6 @@
 from core.color import white, yellow, green
 
 HelpMenu = f"""{white} Otros:
-
     {yellow} help:{green} Imprime este menu, idiota!
     {yellow} banner:{green} Imprime el legendario banner.
     {yellow} clear:{green} Limpia la terminal.
@@ -11,7 +10,6 @@ HelpMenu = f"""{white} Otros:
     {yellow} sherlockupdate:{green} Actualiza el sherlock.
     
 {white} Herramientas:
-
     {yellow} whois:{green} El mejor carcel de whois
     {yellow} ipinfo:{green} Sacar la info de una ip por Osint :)
     {yellow} nslookup:{green} Nslookup, básico.
