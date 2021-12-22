27a7a5bf8fedd304d12173070c6840166c5a0087
! Lil x Sey <92247496+LilxxxSey@users.noreply.github.com>
2021-12-21 17:07:59 -0700

    Update help.py

--- core/help.py
+++ core/help.py
@@ -1,23 +1,23 @@
 from core.color import white, yellow, green, red
 
 HelpMenu = f"""{white} Otros:
-    {green} help:{red} Imprime este menu, idiota!
-    {green} banner:{red} Imprime el legendario banner.
-    {green} clear:{red} Limpia la terminal.
-    {green} exit:{red} Salir o pulsa Ctrl+C
-    {green} update:{red} Actualiza el script.
-    {green} whoami:{red} Hmn, ¿Quién soy?
-    {green} sherlockupdate:{red} Actualiza el sherlock.
+    {yellow} help:{green} Imprime este menu, idiota!
+    {yellow} banner:{green} Imprime el legendario banner.
+    {yellow} clear:{green} Limpia la terminal.
+    {yellow} exit:{green} Salir o pulsa Ctrl+C
+    {yellow} update:{green} Actualiza el script.
+    {yellow} whoami:{green} Hmn, ¿Quién soy?
+    {yellow} sherlockupdate:{green} Actualiza el sherlock.
     
 {white} Herramientas:
-    {green} whois:{red} El mejor carcel de whois
-    {green} ipinfo:{red} Sacar la info de una ip por Osint :)
-    {green} nslookup:{red} Nslookup, básico.
-    {green} phoneinfo:{red} Recolecta info de un número.
-    {green} portscan:{red} Basico Nmap scanner de puertos.
-    {green} passwordcheck:{red} El personal favorito, compruebe si la contraseña ha sido violada antes
-    {green} imagesearch:{red} búsqueda inversa de imágenes de google
-    {green} maclookup:{red} Apuesto a que esto le ahorrará 16 horas de su vida, tratando de buscar una dirección MAC
-    {green} instainfo:{red} ¿Has oído hablar del raspado de Instagram antes?
-    {green} sherlock:{red} ah, me encanta este, busque un nombre de usuario en diferentes plataformas sociales
-    {green} metadata:{red} extrae algunos metadatos de los archivos """
+    {yellow} whois:{green} El mejor carcel de whois
+    {yellow} ipinfo:{green} Sacar la info de una ip por Osint :)
+    {yellow} nslookup:{green} Nslookup, básico.
+    {yellow} phoneinfo:{green} Recolecta info de un número.
+    {yellow} portscan:{green} Basico Nmap scanner de puertos.
+    {yellow} passwordcheck:{green} El personal favorito, compruebe si la contraseña ha sido violada antes
+    {yellow} imagesearch:{green} búsqueda inversa de imágenes de google
+    {yellow} maclookup:{green} Apuesto a que esto le ahorrará 16 horas de su vida, tratando de buscar una dirección MAC
+    {yellow} instainfo:{green} ¿Has oído hablar del raspado de Instagram antes?
+    {yellow} sherlock:{green} ah, me encanta este, busque un nombre de usuario en diferentes plataformas sociales
+    {yellow} metadata:{green} extrae algunos metadatos de los archivos """
